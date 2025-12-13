189e6f4cd65487862579a6932a40a0e310e45e93
myerspat <myerspat@umich.edu>
2025-12-13 09:14:23 -0500

    adding standard deviation

--- notebooks/eigenvalue/pincell/openmc/process.py
+++ notebooks/eigenvalue/pincell/openmc/process.py
@@ -1,12 +1,14 @@
-import openmc
 import matplotlib.pyplot as plt
 import numpy as np
+import openmc
+
+num_groups = 7
 
 # Load statepoint file
 sp = openmc.StatePoint("./statepoint.20000.h5")
 
 # Print eigenvalue
-print(f"keff = {sp.keff}")
+print(f"keff = {sp.keff.nominal_value} +/- {sp.keff.std_dev}")
 
 # Get cell flux tally
 flux = (
@@ -21,13 +23,20 @@ np.save(open("./data/cell_flux.npy", "wb"), flux)
 flux = np.transpose(
     sp.get_tally(name="Regular Mesh")
     .get_values(scores=["flux"])
-    .reshape((128, 128, 7))[..., ::-1],
+    .reshape((128, 128, num_groups))[..., ::-1],
+    axes=(2, 0, 1),
+)
+stdev = np.transpose(
+    sp.get_tally(name="Regular Mesh")
+    .get_values(scores=["flux"], value="std_dev")
+    .reshape((128, 128, num_groups))[..., ::-1],
     axes=(2, 0, 1),
 )
 print(f"Mesh flux shape: {flux.shape}")
 
 # Save data
 np.save(open("./data/mesh_flux.npy", "wb"), flux)
+np.save(open("./data/mesh_stdev.npy", "wb"), stdev)
 
 # Plot fluxes
 X, Y = np.meshgrid(np.linspace(0, 0.63, 129), np.linspace(0, 0.63, 129))
