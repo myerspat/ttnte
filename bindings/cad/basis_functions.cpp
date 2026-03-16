83c85d2ddb723d815f49e2853c60a24ff6805a23
Patrick Myers <myerspat@umich.edu>
2026-03-16 11:34:46 -0400

    resolving conflicts

--- bindings/cad/basis_functions.cpp
+++ bindings/cad/basis_functions.cpp
@@ -1,9 +1,9 @@
 #include "ttnte/cad/basis_functions.hpp"
+#include <optional>
 #include <pybind11/pybind11.h>
 #include <torch/extension.h>
-#include <vector>
 #include <tuple>
-#include <optional>
+#include <vector>
 
 namespace py = pybind11;
 
@@ -11,9 +11,10 @@ void register_BasisFunctions(py::module_& m)
 {
   using BasisFunctions = ttnte::cad::BasisFunctions;
 
-  py::class_<BasisFunctions, std::shared_ptr<BasisFunctions>>(m, 
-    "BasisFunctions")
-    .def(py::init<const std::vector<torch::Tensor>&, const std::vector<int64>&>(),
+  py::class_<BasisFunctions, std::shared_ptr<BasisFunctions>>(
+    m, "BasisFunctions")
+    .def(
+      py::init<const std::vector<torch::Tensor>&, const std::vector<int64>&>(),
       py::arg("knots"), py::arg("degree"))
     .def("find_spans", &BasisFunctions::find_spans)
     .def_readwrite("knots", &BasisFunctions::knots)
